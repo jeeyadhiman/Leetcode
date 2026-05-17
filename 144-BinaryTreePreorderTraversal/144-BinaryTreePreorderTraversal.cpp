b08881036575be2a420f065ee78fb768596ed94a
jeeyadhiman <jeeya.dhiman.2006@gmail.com>
2026-05-17 17:52:11 +0530

    Create 144-BinaryTreePreorderTraversal

---
+++ 144-BinaryTreePreorderTraversal/144-BinaryTreePreorderTraversal.cpp
@@ -0,0 +1,17 @@
+// Last updated: 5/17/2026, 5:52:10 PM
+class Solution {
+public:
+    void preorder(TreeNode* root, vector<int>& ans) {
+        if(root == NULL) return;
+
+        ans.push_back(root->val);       // Root
+        preorder(root->left, ans);      // Left
+        preorder(root->right, ans);     // Right
+    }
+
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        preorder(root, ans);
+        return ans;
+    }
+};
