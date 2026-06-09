c5f3c36b65e177be9d3402cf720f66412543a89c
jeeyadhiman <jeeya.dhiman.2006@gmail.com>
2026-06-09 17:30:07 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -0,0 +1,16 @@
+// Last updated: 6/9/2026, 5:30:07 PM
+class Solution {
+public:
+    int solve(int n, vector<int>& dp){
+        if(n==0 || n==1) return 1;
+
+        if(dp[n] != -1) return dp[n];
+
+        return dp[n] = solve(n-1, dp) + solve(n-2, dp);
+    }
+
+    int climbStairs(int n) {
+        vector<int> dp(n+1, -1);
+        return solve(n, dp);
+    }
+};
